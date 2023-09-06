82cf658feff6721b1b86e98a682ebf0468988cdb
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-09-06 15:40:46 +0200

    add, bfs

--- search/dfs_bfs/src/main.rs
+++ search/dfs_bfs/src/main.rs
@@ -12,8 +12,11 @@ fn main() {
     env_logger::init();
 
     let g = AIGraph::running_example();
-    match dfs(&g) {
-        Ok(path) => println!("Path found: {}", path.join(", ")),
+    match bfs(&g) {
+        Ok(goal_node) => println!(
+            "A path to the goal has been found! Goal node is {}",
+            goal_node
+        ),
         Err(e) => println!("Error: {}", e),
     }
 }
@@ -82,6 +85,70 @@ fn dfs(graph: &AIGraph) -> Result<Vec<AIGraphVId>> {
     bail!("No path found")
 }
 
+fn bfs(graph: &AIGraph) -> Result<AIGraphVId> {
+    let mut eql = HashSet::<AIGraphVId>::new(); // EQL
+    let mut frontier = VecDeque::<AIGraphVId>::new();
+
+    let mut path = Vec::<AIGraphVId>::new();
+
+    info!("Starting BFS...");
+
+    let start_node = match graph.get_start_node() {
+        Some(node) => {
+            info!("{} is the staring node.", node);
+            node
+        }
+        None => {
+            warn!("No start node found.");
+            bail!("No start node found");
+        }
+    };
+
+    info!("Initializing the frontier with {}.", start_node);
+    frontier.push_back(start_node);
+
+    eql.insert(start_node);
+
+    while let Some(node) = frontier.pop_front() {
+        path.push(node);
+
+        info!("Checking if {} is the goal.", node);
+        if graph.is_goal(node) {
+            info!("{} is the goal!", node);
+
+            debug!("Final EQl is now {:?}", eql);
+            debug!("Final Frontier is now {:?}", frontier);
+
+            return Ok(node);
+        }
+
+        info!("Expanding {}.", node);
+        let mut expanded_nodes = graph.expand(node);
+
+        debug!("Expanded nodes are {:?}.", expanded_nodes);
+        info!("Checking if any of the expanded nodes are in the EQL.");
+        expanded_nodes.retain(|node| !eql.contains(node));
+        debug!("Expanded nodes after pruning are {:?}.", expanded_nodes);
+
+        if expanded_nodes.is_empty() {
+            info!("No new nodes found from {}. Backtracking.", node);
+            path.pop();
+            continue;
+        }
+
+        expanded_nodes.sort();
+        for e_node in expanded_nodes {
+            info!("Adding {} to EQL and frontier.", e_node);
+            eql.insert(e_node);
+            frontier.push_back(e_node);
+        }
+
+        debug!("EQl is now {:?}", eql);
+        debug!("Frontier is now {:?}", frontier);
+    }
+    bail!("No path found")
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -103,5 +170,11 @@ mod tests {
     fn check_dfs() {
         let g = AIGraph::running_example();
         assert_eq!(dfs(&g).unwrap().join(", "), "A, B, D, G, E")
+    }
+
+    #[test]
+    fn check_bfs() {
+        let g = AIGraph::running_example();
+        assert_eq!(bfs(&g).unwrap(), "E")
     }
 }
