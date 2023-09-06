83fac64a0c1585e694906243a4efea1c6f6affb1
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-09-06 15:51:09 +0200

    add,refactoring, docs

--- search/dfs_bfs/src/main.rs
+++ search/dfs_bfs/src/main.rs
@@ -12,20 +12,32 @@ fn main() {
     env_logger::init();
 
     let g = AIGraph::running_example();
+
+    match dfs(&g) {
+        Ok(goal_node) => println!(
+            "[DFS] A path to the goal has been found! The goal node is {}.",
+            goal_node
+        ),
+        Err(e) => println!("Error: {}", e),
+    }
+
     match bfs(&g) {
         Ok(goal_node) => println!(
-            "A path to the goal has been found! Goal node is {}",
+            "[BFS] A path to the goal has been found! The goal node is {}.",
             goal_node
         ),
         Err(e) => println!("Error: {}", e),
     }
 }
 
-fn dfs(graph: &AIGraph) -> Result<Vec<AIGraphVId>> {
+/// Performs a Depth-First Search (BFS) with backtracking and pruning on the graph.
+///
+/// # Errors
+///
+/// Returns an error if either no initial node is found or no path is found.
+fn dfs(graph: &AIGraph) -> Result<AIGraphVId> {
     let mut eql = HashSet::<AIGraphVId>::new(); // EQL
     let mut frontier = VecDeque::<AIGraphVId>::new();
-
-    let mut path = Vec::<AIGraphVId>::new();
 
     info!("Starting DFS...");
 
@@ -46,72 +58,6 @@ fn dfs(graph: &AIGraph) -> Result<Vec<AIGraphVId>> {
     eql.insert(start_node);
 
     while let Some(node) = frontier.pop_front() {
-        path.push(node);
-
-        info!("Checking if {} is the goal.", node);
-        if graph.is_goal(node) {
-            info!("{} is the goal!", node);
-
-            debug!("Final EQl is now {:?}", eql);
-            debug!("Final Frontier is now {:?}", frontier);
-
-            return Ok(path);
-        }
-
-        info!("Expanding {}.", node);
-        let mut expanded_nodes = graph.expand(node);
-
-        debug!("Expanded nodes are {:?}.", expanded_nodes);
-        info!("Checking if any of the expanded nodes are in the EQL.");
-        expanded_nodes.retain(|node| !eql.contains(node));
-        debug!("Expanded nodes after pruning are {:?}.", expanded_nodes);
-
-        if expanded_nodes.is_empty() {
-            info!("No new nodes found from {}. Backtracking.", node);
-            path.pop();
-            continue;
-        }
-
-        expanded_nodes.sort();
-        while let Some(e_node) = expanded_nodes.pop() {
-            info!("Adding {} to EQL and frontier.", e_node);
-            eql.insert(e_node);
-            frontier.push_front(e_node);
-        }
-
-        debug!("EQl is now {:?}", eql);
-        debug!("Frontier is now {:?}", frontier);
-    }
-    bail!("No path found")
-}
-
-fn bfs(graph: &AIGraph) -> Result<AIGraphVId> {
-    let mut eql = HashSet::<AIGraphVId>::new(); // EQL
-    let mut frontier = VecDeque::<AIGraphVId>::new();
-
-    let mut path = Vec::<AIGraphVId>::new();
-
-    info!("Starting BFS...");
-
-    let start_node = match graph.get_start_node() {
-        Some(node) => {
-            info!("{} is the staring node.", node);
-            node
-        }
-        None => {
-            warn!("No start node found.");
-            bail!("No start node found");
-        }
-    };
-
-    info!("Initializing the frontier with {}.", start_node);
-    frontier.push_back(start_node);
-
-    eql.insert(start_node);
-
-    while let Some(node) = frontier.pop_front() {
-        path.push(node);
-
         info!("Checking if {} is the goal.", node);
         if graph.is_goal(node) {
             info!("{} is the goal!", node);
@@ -132,7 +78,70 @@ fn bfs(graph: &AIGraph) -> Result<AIGraphVId> {
 
         if expanded_nodes.is_empty() {
             info!("No new nodes found from {}. Backtracking.", node);
-            path.pop();
+            continue;
+        }
+
+        expanded_nodes.sort();
+        while let Some(e_node) = expanded_nodes.pop() {
+            info!("Adding {} to EQL and frontier.", e_node);
+            eql.insert(e_node);
+            frontier.push_front(e_node);
+        }
+
+        debug!("EQl is now {:?}", eql);
+        debug!("Frontier is now {:?}", frontier);
+    }
+    bail!("No path found")
+}
+
+/// Performs a Breadth-First Search (BFS) with backtracking and pruning on the graph.
+///
+/// # Errors
+///
+/// Returns an error if either no initial node is found or no path is found.
+fn bfs(graph: &AIGraph) -> Result<AIGraphVId> {
+    let mut eql = HashSet::<AIGraphVId>::new(); // EQL
+    let mut frontier = VecDeque::<AIGraphVId>::new();
+
+    info!("Starting BFS...");
+
+    let start_node = match graph.get_start_node() {
+        Some(node) => {
+            info!("{} is the staring node.", node);
+            node
+        }
+        None => {
+            warn!("No start node found.");
+            bail!("No start node found");
+        }
+    };
+
+    info!("Initializing the frontier with {}.", start_node);
+    frontier.push_back(start_node);
+
+    eql.insert(start_node);
+
+    while let Some(node) = frontier.pop_front() {
+        info!("Checking if {} is the goal.", node);
+        if graph.is_goal(node) {
+            info!("{} is the goal!", node);
+
+            debug!("Final EQl is now {:?}", eql);
+            debug!("Final Frontier is now {:?}", frontier);
+
+            return Ok(node);
+        }
+
+        info!("Expanding {}.", node);
+        let mut expanded_nodes = graph.expand(node);
+
+        debug!("Expanded nodes are {:?}.", expanded_nodes);
+        info!("Checking if any of the expanded nodes are in the EQL.");
+        expanded_nodes.retain(|node| !eql.contains(node));
+        debug!("Expanded nodes after pruning are {:?}.", expanded_nodes);
+
+        if expanded_nodes.is_empty() {
+            info!("No new nodes found from {}. Backtracking.", node);
             continue;
         }
 
@@ -169,7 +178,7 @@ mod tests {
     #[test]
     fn check_dfs() {
         let g = AIGraph::running_example();
-        assert_eq!(dfs(&g).unwrap().join(", "), "A, B, D, G, E")
+        assert_eq!(dfs(&g).unwrap(), "E")
     }
 
     #[test]
