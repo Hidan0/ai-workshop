0d3e44d4e427a1455bb4b908cd1af2e47c80d9ca
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-09-13 17:19:32 +0200

    wip, added search tree

--- search/dfs_bfs/src/main.rs
+++ search/dfs_bfs/src/main.rs
@@ -1,12 +1,46 @@
-use anyhow::bail;
-use anyhow::Result;
-use graph::ai::AIGraph;
-use graph::ai::AIGraphVId;
-use log::debug;
-use log::info;
-use log::warn;
-use std::collections::HashSet;
-use std::collections::VecDeque;
+use anyhow::{bail, Result};
+use fnv::FnvHashMap;
+use graph::ai::{AIGraph, AIGraphVId, Frontier};
+use log::{debug, info, warn};
+use std::collections::{HashSet, VecDeque};
+
+#[derive(Default, Debug)]
+struct DfsFrontier {
+    f: VecDeque<AIGraphVId>,
+}
+
+impl Frontier for DfsFrontier {
+    fn is_empty(&self) -> bool {
+        self.f.is_empty()
+    }
+
+    fn pop(&mut self) -> Option<AIGraphVId> {
+        self.f.pop_back()
+    }
+
+    fn push(&mut self, node: AIGraphVId) {
+        self.f.push_back(node);
+    }
+}
+
+#[derive(Default, Debug)]
+struct BfsFrontier {
+    f: VecDeque<AIGraphVId>,
+}
+
+impl Frontier for BfsFrontier {
+    fn is_empty(&self) -> bool {
+        self.f.is_empty()
+    }
+
+    fn pop(&mut self) -> Option<AIGraphVId> {
+        self.f.pop_front()
+    }
+
+    fn push(&mut self, node: AIGraphVId) {
+        self.f.push_back(node);
+    }
+}
 
 fn main() {
     env_logger::init();
@@ -30,6 +64,53 @@ fn main() {
     }
 }
 
+fn build_path(tree: &FnvHashMap<AIGraphVId, AIGraphVId>, from: AIGraphVId) -> String {
+    let mut path: Vec<AIGraphVId> = vec![];
+
+    path.push(from);
+    let mut current = from;
+
+    while let Some(next) = tree.get(&current) {
+        path.push(*next);
+        current = *next
+    }
+
+    path.join(" -> ")
+}
+
+fn build_mermaid_graph(tree: &FnvHashMap<AIGraphVId, AIGraphVId>) -> String {
+    let mut id = 0;
+    let mut mermaid_id = FnvHashMap::<AIGraphVId, usize>::default();
+
+    let mut graph = String::new();
+
+    for (node, parent) in tree {
+        if let Some(parent_id) = mermaid_id.get(parent) {
+            graph.push_str(format!("{} --> ", *parent_id).as_str());
+        } else {
+            id += 1;
+            mermaid_id.insert(*parent, id);
+            graph.push_str(format!("{}(({})) --> ", id, parent).as_str());
+        }
+
+        if let Some(node_id) = mermaid_id.get(node) {
+            graph.push_str(format!("{};", *node_id).as_str());
+        } else {
+            id += 1;
+            mermaid_id.insert(*node, id);
+            graph.push_str(format!("{}(({}));", id, node).as_str());
+        }
+    }
+
+    format!(
+        r#"
+    graph TD;
+        {}
+    "#,
+        graph
+    )
+}
+
 /// Performs a Depth-First Search (DFS) with backtracking and pruning on the graph.
 ///
 /// # Errors
@@ -37,7 +118,8 @@ fn main() {
 /// Returns an error if either no initial node is found or no path is found.
 fn dfs(graph: &AIGraph) -> Result<AIGraphVId> {
     let mut eql = HashSet::<AIGraphVId>::new(); // EQL
-    let mut frontier = VecDeque::<AIGraphVId>::new();
+    let mut frontier = DfsFrontier::default();
+    let mut child_of = FnvHashMap::<AIGraphVId, AIGraphVId>::default();
 
     info!("Starting DFS...");
 
@@ -53,17 +135,20 @@ fn dfs(graph: &AIGraph) -> Result<AIGraphVId> {
     };
 
     info!("Initializing the frontier with {}.", start_node);
-    frontier.push_back(start_node);
+    frontier.push(start_node);
 
     eql.insert(start_node);
 
-    while let Some(node) = frontier.pop_front() {
+    while let Some(node) = frontier.pop() {
         info!("Checking if {} is the goal.", node);
         if graph.is_goal(node) {
             info!("{} is the goal!", node);
 
             debug!("Final EQl is now {:?}", eql);
             debug!("Final Frontier is now {:?}", frontier);
+
+            println!("{}", build_path(&child_of, node));
+            println!("{}", build_mermaid_graph(&child_of));
 
             return Ok(node);
         }
@@ -85,7 +170,10 @@ fn dfs(graph: &AIGraph) -> Result<AIGraphVId> {
         while let Some(e_node) = expanded_nodes.pop() {
             info!("Adding {} to EQL and frontier.", e_node);
             eql.insert(e_node);
-            frontier.push_front(e_node);
+
+            child_of.insert(e_node, node);
+
+            frontier.push(e_node);
         }
 
         debug!("EQl is now {:?}", eql);
@@ -101,7 +189,8 @@ fn dfs(graph: &AIGraph) -> Result<AIGraphVId> {
 /// Returns an error if either no initial node is found or no path is found.
 fn bfs(graph: &AIGraph) -> Result<AIGraphVId> {
     let mut eql = HashSet::<AIGraphVId>::new(); // EQL
-    let mut frontier = VecDeque::<AIGraphVId>::new();
+    let mut frontier = BfsFrontier::default();
+    let mut child_of = FnvHashMap::<AIGraphVId, AIGraphVId>::default();
 
     info!("Starting BFS...");
 
@@ -117,17 +206,20 @@ fn bfs(graph: &AIGraph) -> Result<AIGraphVId> {
     };
 
     info!("Initializing the frontier with {}.", start_node);
-    frontier.push_back(start_node);
+    frontier.push(start_node);
 
     eql.insert(start_node);
 
-    while let Some(node) = frontier.pop_front() {
+    while let Some(node) = frontier.pop() {
         info!("Checking if {} is the goal.", node);
         if graph.is_goal(node) {
             info!("{} is the goal!", node);
 
             debug!("Final EQl is now {:?}", eql);
             debug!("Final Frontier is now {:?}", frontier);
+
+            println!("{}", build_path(&child_of, node));
+            println!("{}", build_mermaid_graph(&child_of));
 
             return Ok(node);
         }
@@ -149,7 +241,10 @@ fn bfs(graph: &AIGraph) -> Result<AIGraphVId> {
         for e_node in expanded_nodes {
             info!("Adding {} to EQL and frontier.", e_node);
             eql.insert(e_node);
-            frontier.push_back(e_node);
+
+            child_of.insert(e_node, node);
+
+            frontier.push(e_node);
         }
 
         debug!("EQl is now {:?}", eql);
